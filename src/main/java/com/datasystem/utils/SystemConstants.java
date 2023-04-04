a4ef80ad1edf11429d5a3d20fc216a16b6da3a98
Pauluooooo <35886796+Pauluooooo@users.noreply.github.com>
2023-04-04 17:05:49 +0800

    Update SystemConstants.java

--- src/main/java/com/datasystem/utils/SystemConstants.java
+++ src/main/java/com/datasystem/utils/SystemConstants.java
@@ -7,8 +7,8 @@ package com.datasystem.utils;
  */
 
 public class SystemConstants {
-  public static final String ACCESS_KEY = "LTAI5tERJ3K2mpDVnXEE66f8";
-  public static final String ACCESSKEY_SECRET = "awmamuEFPtuoNPtvzRt2qb8wd5QYOr";
+  public static final String ACCESS_KEY = "xxxxxx";
+  public static final String ACCESSKEY_SECRET = "xxxxxxx";
   public static final Integer SEND_CODE_STATUS = 200;
   public static final String LOGIN_CODE_KEY = "login:code";
   public static final Long LOGIN_CODE_TTL = 5L;
